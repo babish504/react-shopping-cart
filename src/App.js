5b7c79780892a16db08cd432edbd5d2bb089644a
Babish Adapa <baadapa@microsoft.com>
2020-12-12 07:16:27 +0530

    Product list feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
-function App() {
+import Products from "./components/Products";
+import data from "./data.json";
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">React shopping card</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products}></Products>
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
       </main>
       <footer>
         All rights reserved!
       </footer>
     </div>
   );
+}
 }
 
 export default App;
